9922b0f086b6b8c72fa53848070f0d4332149cdc
Rob Levin <roblevintennis@gmail.com>
2019-04-18 15:02:30 -0700

    Went through the flow of updating a snapshot. Created  script for this too.

--- src/lib/jest/link.jsx
+++ src/lib/jest/link.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Link extends React.Component {
   render() {
     return (
-      <a href={this.props.page || '#'}>
+      <a className="foo" href={this.props.page || '#'}>
         {this.props.children}
       </a>
     );
